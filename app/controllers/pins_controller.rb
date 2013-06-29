b4ea5fa512c2eeeb1bf84c1dec4f6321f371856f
Tom Mudgett <tomem23@gmail.com>
2013-06-29 16:03:11 -0400

    Corrections to pins_controller.rb

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -36,7 +36,7 @@ class PinsController < ApplicationController
 
   # GET /pins/1/edit
   def edit
-    @pin = current_user.pins.new.find(params[:id])
+    @pin = current_user.pins.find(params[:id])
   end
 
   # POST /pins
@@ -58,7 +58,7 @@ class PinsController < ApplicationController
   # PUT /pins/1
   # PUT /pins/1.json
   def update
-    @pin = current_user.pins.new.find(params[:id])
+    @pin = current_user.pins.find(params[:id])
 
     respond_to do |format|
       if @pin.update_attributes(params[:pin])
@@ -74,7 +74,7 @@ class PinsController < ApplicationController
   # DELETE /pins/1
   # DELETE /pins/1.json
   def destroy
-    @pin = current_user.pins.new.find(params[:id])
+    @pin = current_user.pins.find(params[:id])
     @pin.destroy
 
     respond_to do |format|
